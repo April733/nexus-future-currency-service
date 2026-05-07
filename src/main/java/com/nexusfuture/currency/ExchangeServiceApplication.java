5c440d8cbc9b682493e552b286fcb11d1af513dc
april <apirl1325439733@gmail.com>
2026-05-07 16:40:35 +0700

    test

--- src/main/java/com/nexusfuture/currency/ExchangeServiceApplication.java
+++ src/main/java/com/nexusfuture/currency/ExchangeServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // 开启定时器
+@EnableScheduling //   开启定时器
 public class ExchangeServiceApplication {
 
     public static void main(String[] args) {
